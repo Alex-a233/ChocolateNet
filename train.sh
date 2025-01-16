90401ea821ee54ef452e52edaa69a77efb3adc1d
Alex~ <807680313@qq.com>
2025-01-16 08:31:16 +0800

    1. some changes

---
+++ train.sh
@@ -0,0 +1,2 @@
+# shell for training model
+python -W ignore train.py
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -14,70 +14,10 @@ from torchvision import transforms as T
 
 class TrainSet(Dataset):
 
-    # def __init__(self, args):
-    #     image_path = os.path.join(args.train_path, 'images')
-    #     mask_path = os.path.join(args.train_path, 'masks')
-    #     self.images = [os.path.join(image_path, f) for f in os.listdir(image_path) if f.endswith('.png')]
-    #     self.masks = [os.path.join(mask_path, f) for f in os.listdir(mask_path) if f.endswith('.png')]
-    #     self.images = sorted(self.images)
-    #     self.masks = sorted(self.masks)
-    #     self.size = len(self.images)
-    #     self.use_aug = args.use_aug
-    #     self.train_size = args.train_size
-    #
-    #     if self.use_aug:
-    #         self.image_transform = T.Compose([
-    #             T.RandomRotation(90, expand=False, center=None, fill=None),
-    #             T.RandomHorizontalFlip(),
-    #             T.RandomVerticalFlip(),
-    #             T.Resize((self.train_size, self.train_size)),
-    #             T.ToTensor(),
-    #             T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # ImageNet的通道级标准化参数
-    #         ])
-    #         self.mask_transform = T.Compose([
-    #             T.RandomRotation(90, expand=False, center=None, fill=None),
-    #             T.RandomHorizontalFlip(),
-    #             T.RandomVerticalFlip(),
-    #             T.Resize((self.train_size, self.train_size)),
-    #             T.ToTensor()
-    #         ])
-    #
-    #     else:
-    #         self.image_transform = T.Compose([
-    #             T.Resize((self.train_size, self.train_size)),
-    #             T.ToTensor(),
-    #             T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    #         ])
-    #         self.mask_transform = T.Compose([
-    #             T.Resize((self.train_size, self.train_size)),
-    #             T.ToTensor()
-    #         ])
-    #
-    # def __getitem__(self, index):
-    #     image = Image.open(self.images[index]).convert('RGB')
-    #     mask = Image.open(self.masks[index]).convert('L')
-    #     # TODO: 染色增强策略—>息肉染色分明边界
-    #
-    #     seed = random.randint(2025)
-    #
-    #     random.seed(seed)
-    #     torch.manual_seed(seed)
-    #     torch.cuda.manual_seed(seed)
-    #     image = self.image_transform(image)
-    #
-    #     random.seed(seed)
-    #     torch.manual_seed(seed)
-    #     torch.cuda.manual_seed(seed)
-    #     mask = self.mask_transform(mask)
-    #     return image, mask
-    #
-    # def __len__(self):
-    #     return self.size
-
     def __init__(self, args):
-        self.image_root = os.path.join(args.train_path, 'images')
-        self.gt_root = os.path.join(args.train_path, 'masks')
-        self.samples = [name for name in os.listdir(self.image_root) if name[0] != "."]
+        self.image_path = os.path.join(args.train_path, 'images')
+        self.mask_path = os.path.join(args.train_path, 'masks')
+        self.images = [name for name in os.listdir(self.image_path) if name[0] != "."]
         self.transform = A.Compose([
             A.Normalize(),
             A.Resize(args.train_size, args.train_size, interpolation=cv2.INTER_NEAREST),
@@ -88,33 +28,35 @@ class TrainSet(Dataset):
         ])
 
         self.color1, self.color2 = [], []
-        for name in self.samples:
-            if name[:-4].isdigit():
+        for name in self.images:
+            if name[:-4].isdigit():  # ClinicDB
                 self.color1.append(name)
-            else:
+            else:  # Kvasir
                 self.color2.append(name)
 
-    def __getitem__(self, idx):  # TODO: try SANet's Color Exchange Strategy
-        name = self.samples[idx]
-        image = cv2.imread(self.image_root + '/' + name)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-        # TODO: change p = 1/2, delay first wanna test if there is no init in attn.py
-        name2 = self.color1[idx % len(self.color1)] if np.random.rand() < 0.7 else self.color2[idx % len(self.color2)]
-        image2 = cv2.imread(self.image_root + '/' + name2)
+    def __getitem__(self, i):  # TODO: try SANet's Color Exchange Strategy
+        name = self.images[i]
+        image1 = cv2.imread(self.image_path + '/' + name)
+        image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2LAB)
+        # TODO: change p, delay first wanna test if there is no init in attn.py
+        name2 = self.color1[i % len(self.color1)] if np.random.rand() > 0.7 else self.color2[i % len(self.color2)]
+        image2 = cv2.imread(self.image_path + '/' + name2)
         image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2LAB)
 
-        mean, std = image.mean(axis=(0, 1), keepdims=True), image.std(axis=(0, 1), keepdims=True)
+        mean1, std1 = image1.mean(axis=(0, 1), keepdims=True), image1.std(axis=(0, 1), keepdims=True)
         mean2, std2 = image2.mean(axis=(0, 1), keepdims=True), image2.std(axis=(0, 1), keepdims=True)
 
-        image = np.uint8((image - mean) / std * std2 + mean2)
+        image = np.uint8((image1 - mean1) / std1 * std2 + mean2)
         image = cv2.cvtColor(image, cv2.COLOR_LAB2RGB)
-        mask = cv2.imread(self.gt_root + '/' + name, cv2.IMREAD_GRAYSCALE) / 255.0
+        # dye strategy v1
+        # image = cv2.applyColorMap(image, 5)
+        mask = cv2.imread(self.mask_path + '/' + name, cv2.IMREAD_GRAYSCALE) / 255.0
         pair = self.transform(image=image, mask=mask)
         pair['mask'] = torch.unsqueeze(pair['mask'], 0)
         return pair['image'], pair['mask']
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.images)
 
 
 class TestSet:
